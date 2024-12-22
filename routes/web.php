e022e6beabf315f5fc21ad0d70a685f296ed0b36
1Ly45 <137293380+1Ly45@users.noreply.github.com>
2024-12-23 00:56:23 +0700

    Rename Layanan to About

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PemesananController;
-use App\Http\Controllers\LayananController;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\KontakController;
 use App\Http\Controllers\BerandaController;
 use App\Http\Controllers\DashboardController;
 
 Route::get('/', [BerandaController::class, 'index'])->name('beranda');
-Route::get('/layanan', [LayananController::class, 'index'])->name('layanan');
+Route::get('/profile', [ProfileController::class, 'index'])->name('layanan');
 Route::get('/pemesanan', [PemesananController::class, 'index'])->name('pemesanan');
 Route::get('/pemesanan/{slug}', [PemesananController::class, 'show'])->name('menu.show');
 Route::get('/kontak', [KontakController::class, 'index'])->name('kontak');
